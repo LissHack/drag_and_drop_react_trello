a30640f023c6dfd0979b52b6e2f5e2b158943a40
Liss <les.karo@yandex.ru>
2023-06-05 19:54:44 +0400

    написание логики компонента

--- src/App.js
+++ src/App.js
@@ -18,21 +18,22 @@ const App = () => {
             title: 'Сделано',
             items: [{id: 7, title: 'Сделать уборку',}, {id: 8, title: 'Поспать',}, {id: 9, title: 'Убраться',}]
         },
-
     ])
 
-    function dragStartHandler(e, board, item) {
+    const [currentBoard, setCurrentBoard] = useState(null)
+    const [currentItem, setCurrentItem] = useState(null)
 
+    function dragStartHandler(e, board, item) {
+        setCurrentBoard(board)
+        setCurrentItem(item)
     }
 
     function dragLeaveHandler(e) {
         e.target.style.boxShadow = 'none'
-
     }
 
     function dragEndHandler(e) {
         e.target.style.boxShadow = 'none'
-
     }
 
     function dragOverHandler(e) {
@@ -44,6 +45,19 @@ const App = () => {
 
     function dropHandler(e, board, item) {
         e.preventDefault()
+        const currentIndex = currentBoard.items.indexOf(currentItem)
+        currentBoard.items.splice(currentIndex, 1)
+        const dropIndex = board.items.indexOf(item)
+        board.items.splice(dropIndex + 1, 0, currentItem)
+        setBoards(boards.map(b => {
+            if (b.id === board.id){
+                return board
+            }
+            if (b.id === currentBoard.id){
+                return currentBoard
+            }
+            return b
+        }))
     }
 
 
@@ -54,10 +68,10 @@ const App = () => {
                     <div className="board__title">{board.title}</div>
                     {board.items.map(item =>
                         <div
-                            onDragStart={(e) => dragStartHandler(e, board, item)}
-                            onDragLeave={(e) => dragLeaveHandler(e)}
-                            onDragEnd={(e) => dragEndHandler(e)}
                             onDragOver={(e) => dragOverHandler(e)}
+                            onDragLeave={(e) => dragLeaveHandler(e)}
+                            onDragStart={(e) => dragStartHandler(e, board, item)}
+                            onDragEnd={(e) => dragEndHandler(e)}
                             onDrop={(e) => dropHandler(e, board, item)}
                             draggable={true}
                             className='item'
