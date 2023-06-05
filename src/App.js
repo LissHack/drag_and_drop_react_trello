70709873f1e69a6bd1562b54c8d376514f8fe2e7
Liss <les.karo@yandex.ru>
2023-06-05 19:58:58 +0400

    реализация переноса карточки на пустую доску

--- src/App.js
+++ src/App.js
@@ -50,10 +50,10 @@ const App = () => {
         const dropIndex = board.items.indexOf(item)
         board.items.splice(dropIndex + 1, 0, currentItem)
         setBoards(boards.map(b => {
-            if (b.id === board.id){
+            if (b.id === board.id) {
                 return board
             }
-            if (b.id === currentBoard.id){
+            if (b.id === currentBoard.id) {
                 return currentBoard
             }
             return b
@@ -61,10 +61,29 @@ const App = () => {
     }
 
 
+    function dropCardHandler(e, board) {
+        board.items.push(currentItem)
+        const currentIndex = currentBoard.items.indexOf(currentItem)
+        currentBoard.items.splice(currentIndex, 1)
+        setBoards(boards.map(b => {
+            if (b.id === board.id) {
+                return board
+            }
+            if (b.id === currentBoard.id) {
+                return currentBoard
+            }
+            return b
+        }))
+    }
+
     return (
         <div className='app'>
             {boards.map(board =>
-                <div className='board'>
+                <div
+                    className='board'
+                    onDragOver={(e) => dragOverHandler(e)}
+                    onDrop={(e) => dropCardHandler(e, board)}
+                >
                     <div className="board__title">{board.title}</div>
                     {board.items.map(item =>
                         <div
